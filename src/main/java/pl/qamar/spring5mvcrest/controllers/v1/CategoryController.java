44cf6841873467ca85adee640916de8d9176ac25
Konrad Jaroslawski <kgj.mails@gmail.com>
2019-11-30 17:32:15 +0100

    RestController and ResponseStatus annotations refactored

--- src/main/java/pl/qamar/spring5mvcrest/controllers/v1/CategoryController.java
+++ src/main/java/pl/qamar/spring5mvcrest/controllers/v1/CategoryController.java
@@ -2,15 +2,16 @@ package pl.qamar.spring5mvcrest.controllers.v1;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import pl.qamar.spring5mvcrest.api.v1.model.CategoryDTO;
 import pl.qamar.spring5mvcrest.api.v1.model.CategoryListDTO;
 import pl.qamar.spring5mvcrest.services.CategoryService;
 
-@Controller
+@RestController
 @RequestMapping(CategoryController.BASE_URL)
 public class CategoryController {
 
@@ -23,12 +24,14 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<CategoryListDTO> getAllCategories() {
-        return new ResponseEntity<>(new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDTO getAllCategories() {
+        return new CategoryListDTO(categoryService.getAllCategories());
     }
 
     @GetMapping("{name}")
-    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
-        return new ResponseEntity<>(categoryService.getCategoryByName(name), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name) {
+        return categoryService.getCategoryByName(name);
     }
 }
